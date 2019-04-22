aea30a30648c2dc42e1b14434c616be916ec0db4
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-22 18:12:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT name, age, SUM(pledges.amount) 
-  FROM pledges 
+  FROM users 
   INNER JOIN pledges 
   ON user.id = pledges.id 
   GROUP BY users.name 
