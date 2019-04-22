c2eb120e65e3c38c0bdfe33ddd0f0b9625d33638
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-22 18:34:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,8 +38,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   ON users.name = pledges.user_id
   GROUP BY users.name
   ORDER BY pledges.amount
-  ORDER BY us
-  ;"
+  ORDER BY users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
