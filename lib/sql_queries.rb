e9bad217f38dece7515791da6cb41ea8ab6e9152
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-22 17:33:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT title, SUM(amount) 
   FROM projects 
-  JOIN pledges 
+  INNER JOIN pledges 
   ON projects.id = pledges.id 
   GROUP BY projects.id 
   ORDER by projects.title;"
