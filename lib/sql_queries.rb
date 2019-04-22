d149e8a9a961c6f95e950bd1dcd3e470c9f57588
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-22 17:27:54 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   JOIN pledges 
   ON user.id = pledges.id 
   GROUP BY user.name 
-  ORDER by user.title;"
+  ORDER by user.age;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
